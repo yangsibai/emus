e724b43b61a0049d185fc79d55457e9237ef2c36
Yangsibai <yangsibai@gmail.com>
2016-03-24 21:25:55 +0800

    add current

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,6 +1,7 @@
 $(function () {
     var $iframe = $('#iframe');
     $('li').click(function (){
+        $(this).addClass("current").siblings().removeClass("current");
         var pageID = $(this).data('page-id');
         $iframe.attr("src", "/page/" + pageID);
     });
